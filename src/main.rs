12cfc0b022af22247f16145c5ed06be17acc69b4
Sigvard Johansen Seljelv <{ID}+{username}@users.noreply.github.com>
2023-12-02 16:18:20 +0100

    Implement the use of day fetcher in main

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
-mod file_reader;
-mod day_interface;
-mod day1;
-mod day2;
 
-use day2::DayTwo;
-use day_interface::DayInterface;
+pub mod days;
+pub mod utils;
+
+
+use crate::utils::file_reader;
+use crate::days::day_fetch::fetch_day;
 
 fn main() {
-    let file_path = "input/day2.txt";
-    match file_reader::read_file_lines(file_path){
-        Ok(lines) => {
-            let day = DayTwo;
-            day.solve_puzzle_one(&lines);
-            day.solve_puzzle_two(&lines);
+    let day_to_run = "2";
+    let file_path = format!("input/day{}.txt", day_to_run);
+    match fetch_day(day_to_run){
+        Ok(day) => {
+            match file_reader::read_file_lines(&file_path){
+                Ok(lines) => {
+                    day.solve_puzzle_one(&lines);
+                    day.solve_puzzle_two(&lines);
+                }
+                Err(e) => println!("Error reading file: {}", e),
+            }
         }
-        Err(e) => println!("Error reading file: {}", e),
+        Err(error) => print!("Error {}", error)
     }
+
 }
